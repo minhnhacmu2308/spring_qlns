da213002ce9d333277dca2da47cf4d9e8df0d3ec
Nguyễn Minh Nhả <nguyenminhnha@MacBook-Pro-cua-Nguyen.local>
2023-05-07 21:45:18 +0700

    first commit

---
+++ src/main/java/com/qlns/spring_qlns/SpringQlnsApplication.java
@@ -0,0 +1,13 @@
+package com.qlns.spring_qlns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQlnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringQlnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qlns/spring_qlns/SpringQlnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qlns.spring_qlns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQlnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
